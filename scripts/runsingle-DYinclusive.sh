63016bedb57fe0d7ece780c1999811d9027d6e39
Margaret Lazarovits <mrlazarovits@gmail.com>
2019-10-21 14:47:02 -0500

    runsingle file for dy inclusive

---
+++ scripts/runsingle-DYinclusive.sh
@@ -0,0 +1,7 @@
+
+cd ..
+./MakeReducedNtuple.x -ifile=root://cmseos.fnal.gov//eos/uscms/store/user/lpcsusyhad/Stop_production/Autumn18_102X_v1/PreProcessed_22March2019/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/2018_Data_RunIIAutumn18MiniAOD-102X_v15-v1/190326_032843/0000/prod2018MC_NANO_1-1.root -selector=prod2018MC_reducedNANO_Triggers -ofile=./output/prod2018_reducedNANO_Triggers_DYinclusive.root
+
+./MakeReducedNtuple.x -ifile=root://cmseos.fnal.gov//eos/uscms/store/user/lpcsusyhad/Stop_production/Fall17_94X_v2_NanAOD_MC/PreProcessed_15Jan2019/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/2017_MC_RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_v14-v1-ext1/190130_181315/0000/prod2017MC_NANO_1-1.root -selector=prod2017MC_reducedNANO_Triggers -ofile=./output/prod2017MC_reducedNANO_Triggers_DYinclusive.root
+
+./MakeReducedNtuple.x -ifile=root://cmseos.fnal.gov//eos/uscms/store/user/lpcsusyhad/Stop_production/Summer16_94X_v3/PreProcessed_22Feb2019/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/2016_MINIAODv3_RunIISummer16MiniAODv3-PUMoriond17_94X_v3-v2-ext1/190301_191048/0000/prod2016MC_NANO_1.root -selector=prod2016MC_reducedNANO_Triggers -ofile=./output/prod2018_reducedNANO_Triggers_DYinclusive.root
