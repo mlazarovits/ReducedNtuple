ae61a2f35895a9926d6b31679152b03bf3459164
Margaret Lazarovits <mlazarovits@gmail.com>
2020-01-06 14:10:36 -0500

    check eff w/o med iso id

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -223,7 +223,7 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 	}
 
 	nEntries = m_tree->GetEntries();
-	for(int evt = 0; evt < 100000; evt++){
+	for(int evt = 0; evt < nEntries; evt++){
 		m_tree->GetEntry(evt);
 		if (evt % 1000 == 0) {
 			fprintf(stdout, "\r  Processed events: %8d of %8d ", evt, nEntries);
