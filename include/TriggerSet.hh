5aa25b490196f1f9d0e86431d302f3511a08968d
Margaret Lazarovits <mlazarovits@gmail.com>
2020-01-22 17:56:23 -0600

    wjets17 single muon loose miniIsoVeryTight

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -238,7 +238,7 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 					if(l_Muon_mediumId->GetValue(i) == true) MuonmediumId_counter += 1;
 					else if(l_Muon_tightId->GetValue(i) == true) MuontightId_counter += 1;
 					else if(l_Muon_mediumPromptId->GetValue(i) == true) MuonmedpromptId_counter += 1;
-					if(l_Muon_miniIsoId->GetValue(i) == 2) muonminiIso_counter += 1; //1=MiniIsoLoose, 2=MiniIsoMedium, 3=MiniIsoTight, 4=MiniIsoVeryTight
+					if(l_Muon_miniIsoId->GetValue(i) == 4) muonminiIso_counter += 1; //1=MiniIsoLoose, 2=MiniIsoMedium, 3=MiniIsoTight, 4=MiniIsoVeryTight
 					// else continue;	
 				}
 
