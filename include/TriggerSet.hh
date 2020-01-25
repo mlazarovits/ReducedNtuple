f05221adc45f8656862a32b1d0257d7acf3e5a9b
Margaret Lazarovits <mlazarovits@gmail.com>
2020-01-25 13:29:28 -0600

    debug

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -174,7 +174,7 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 	TLeaf* l_Muon_tightId = m_tree->GetLeaf("Muon_tightId");
 	TLeaf* l_Muon_miniIsoId = m_tree->GetLeaf("Muon_miniIsoId");
 	TLeaf* l_Muon_pfRelIso03_all = m_tree->GetLeaf("Muon_pfRelIso03_all");
-	TLeaf* l_Muon_minipfRelIso_all = m_tree->GetLeaf("Muon_minipfRelIso_all");
+	TLeaf* l_Muon_minipfRelIso_all = m_tree->GetLeaf("Muon_miniPFRelIso_all");
 
 	TLeaf* l_nElectron = m_tree->GetLeaf("nElectron");
 	TLeaf* l_Electron_pfRelIso03_all = m_tree->GetLeaf("Muon_pfRelIso03_all");
@@ -254,7 +254,6 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 					// if(l_Muon_miniIsoId->GetValue(i) == 4) muonminiIso_counter += 1; //1=MiniIsoLoose, 2=MiniIsoMedium, 3=MiniIsoTight, 4=MiniIsoVeryTight
 					if(l_Muon_pfRelIso03_all->GetValue(i) < 0.1) muonpfRelIso03_counter += 1;
 					if(l_Muon_minipfRelIso_all->GetValue(i) < 0.1) muonminipfRelIso_counter += 1;
-					// else continue;	
 				}
 
 				// if(MuonmediumId_counter != 1) continue;  //exactly 1 mediumId muon
