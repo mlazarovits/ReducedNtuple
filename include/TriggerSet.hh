18d16010509542b202561e7fc03ec34fd3d100d4
Margaret Lazarovits <mlazarovits@gmail.com>
2020-01-28 16:22:01 -0600

    mini Iso rel

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -254,7 +254,7 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 					else if(l_Muon_mediumPromptId->GetValue(i) == true) MuonmedpromptId_counter += 1;
 					// if(l_Muon_miniIsoId->GetValue(i) == 4) muonminiIso_counter += 1; //1=MiniIsoLoose, 2=MiniIsoMedium, 3=MiniIsoTight, 4=MiniIsoVeryTight
 					// if(l_Muon_pfRelIso03_all->GetValue(i) < 0.1) muonpfRelIso03_counter += 1;
-					if(l_Muon_minipfRelIso_all->GetValue(i) < 0.1) cout << l_Muon_minipfRelIso_all->GetValue(i) << endl;//muonminipfRelIso_counter += 1;
+					if(l_Muon_minipfRelIso_all->GetValue(i) < 0.1) muonminipfRelIso_counter += 1;
 				}
 
 				// cout << l_Muon_minipfRelIso_all->GetValue() << endl;
@@ -263,7 +263,7 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 				// if(MuontightId_counter != 1) continue; //exactly 1 tightId muon
 				// if(nTrig == 0 || nTrig == 1){ //only apply iso selection to triggers with that
 					// if(muonpfRelIso03_counter != 1) continue;
-					if(muonminiIso_counter != 1){
+					if(muonminipfRelIso_counter != 1){
 						continue; //exactly 1 miniIsoId muon
 					}
 					// else{
