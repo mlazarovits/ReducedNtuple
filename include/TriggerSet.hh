5998ff7f5deac35e826ddb8d56baa28a272b4c6c
Margaret Lazarovits <mlazarovits@gmail.com>
2020-01-28 16:20:19 -0600

    mini Iso rel

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -246,7 +246,7 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 				int muonpfRelIso03_counter = 0;
 				int muonminipfRelIso_counter = 0;
 
-
+				cout << nMuon << endl;
 				for(int i = 0; i < nMuon; i++){
 					// cout << "med id: " << l_Muon_mediumId->GetValue(i) << endl;
 					if(l_Muon_mediumId->GetValue(i) == true) MuonmediumId_counter += 1;
@@ -254,7 +254,7 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 					else if(l_Muon_mediumPromptId->GetValue(i) == true) MuonmedpromptId_counter += 1;
 					// if(l_Muon_miniIsoId->GetValue(i) == 4) muonminiIso_counter += 1; //1=MiniIsoLoose, 2=MiniIsoMedium, 3=MiniIsoTight, 4=MiniIsoVeryTight
 					// if(l_Muon_pfRelIso03_all->GetValue(i) < 0.1) muonpfRelIso03_counter += 1;
-					if(l_Muon_minipfRelIso_all->GetValue(i) < 0.1) cout << l_Muon_minipfRelIso_all->GetValue(i) << "here" << endl;//muonminipfRelIso_counter += 1;
+					if(l_Muon_minipfRelIso_all->GetValue(i) < 0.1) cout << l_Muon_minipfRelIso_all->GetValue(i) << endl;//muonminipfRelIso_counter += 1;
 				}
 
 				// cout << l_Muon_minipfRelIso_all->GetValue() << endl;
