f852e1c48a1f4a937dca3587e500b1fa831be1f0
Margaret Lazarovits <mlazarovits@gmail.com>
2020-02-11 10:48:50 -0600

    test new functions

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -268,7 +268,7 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 		vec_ltrig.push_back(l_trig);
 	}
 
-	if(debug == true) nEntries = 1E1;
+	if(debug == true) nEntries = 1E6;
 	else if (debug == false) nEntries = m_tree->GetEntries();
 	for(int evt = 0; evt < nEntries/10; evt++){
 		m_tree->GetEntry(evt);
