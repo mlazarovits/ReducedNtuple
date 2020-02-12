052533143779d30b67e13ecac1d2e6d9e67716ba
Margaret Lazarovits <mlazarovits@gmail.com>
2020-02-11 18:25:37 -0600

    test eta

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -295,7 +295,7 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 	if(strstr(m_var.c_str(),"eta")){
 		nBins = 40;
 		cout << "here 1" << endl;
-		effbins.at(0) = 0.0;
+		effbins.push_back( 0.0);
 		for(int i = 1; i < nBins+2; i++){
 			effbins.push_back(effbins.at(i-1) + 0.2);
 			// effbins[i] = effbins[i-1] + 0.1;
