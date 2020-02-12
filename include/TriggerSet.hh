6dd8bf4d392a5c038b37d02654a7520b20c2c799
Margaret Lazarovits <mlazarovits@gmail.com>
2020-02-12 14:12:37 -0600

    test eta and minireliso cuts

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -357,7 +357,7 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 				if(abs(muonSelections.at(NmuonSelections-1)) > 2.5) continue;
 			}
 
-			// if(muonSelection.at(4) > 0.1) iso = false;
+			if(muonSelection.at(4) > 0.1) continue;
 
 
 
