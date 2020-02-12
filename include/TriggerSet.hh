08b29302abfab2b8fea6709bdac2ff00b2351696
Margaret Lazarovits <mlazarovits@gmail.com>
2020-02-12 14:50:18 -0600

    test eta and minireliso cuts

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -360,17 +360,9 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 			// 	if(abs(muonSelections.at(NmuonSelections-1)) > 2.5) continue;
 			// }
 
-			if(muonSelections.at(4) < 1){
-
-				iso = false;
-				// continue;
-			}
-
-		
+			if(muonSelections.at(4) < 1) iso = false;
+				
 			
-
-
-
 
 		}
 
@@ -393,10 +385,9 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 			}
 
 		for(int nTrig = 0; nTrig < m_triggers.size(); nTrig++){
-			// if(strstr(m_triggers.at(nTrig).c_str(),"Iso")){
-			// 	// if(iso) cout << "iso: " << iso << endl;
-			// 	if(!iso) continue;
-			// }
+			if(strstr(m_triggers.at(nTrig).c_str(),"Iso")){
+				if(!iso) continue;
+			}
 			bool bPassed = vec_ltrig.at(nTrig)->GetValue();
 			// cout << bPassed << endl;
 			vec_eff.at(nTrig)->Fill((bPassed),l_var->GetValue());
