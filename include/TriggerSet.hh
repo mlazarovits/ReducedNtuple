00de180357cc2ba5e4e574bf5e770fbf0e906c77
Margaret Lazarovits <mlazarovits@gmail.com>
2020-02-11 18:40:03 -0600

    test eta

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -319,7 +319,7 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 		vec_ltrig.push_back(l_trig);
 	}
 
-	if(debug == true) nEntries = 1E7;
+	if(debug == true) nEntries = 1E8;
 	else if (debug == false) nEntries = m_tree->GetEntries();
 	
 	for(int evt = 0; evt < nEntries/10; evt++){
