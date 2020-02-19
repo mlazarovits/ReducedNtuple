06c7e6c104436d5b11b12367047dc4d2834ae429
Margaret Lazarovits <mlazarovits@gmail.com>
2020-02-19 10:36:09 -0600

    effbins tweaking

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -313,14 +313,14 @@ inline std::vector<Double_t> TriggerSet::makeEffBins(){
 	std::vector<Double_t> effbins;
 	//set bins of TEff object
 	if(strstr(m_var.c_str(),"pt")){
-		nBins = 40;
+		nBins = 60;
 		effbins.push_back(0.0);
 		for(int i = 1; i < nBins/2 + 1; i++){
 			effbins.push_back(effbins.at(i-1) + 0.5);
 			// cout << effbins[i] << endl;
 		}
 		for(int i = nBins/2 + 1; i < nBins+2; i++){
-			effbins.push_back(effbins.at(i-1) + 1.0);
+			effbins.push_back(effbins.at(i-1) + 0.5);
 			// cout << effbins[i] << endl;
 		}
 	}
@@ -352,7 +352,6 @@ inline vector<TEfficiency*> TriggerSet::Analyze(){
 
 
 	vector<Double_t> effbins = makeEffBins();
-	cout << effbins.size() << endl;
 	Int_t nBins = effbins.size()-2;
 
 
