f9efa97e82c3c58ac623e01403e9fa4d69a9b802
Margaret Lazarovits <mlazarovits@gmail.com>
2020-03-01 19:26:23 -0600

    test 2d plot

--- include/TriggerSet.hh
+++ include/TriggerSet.hh
@@ -702,6 +702,7 @@ inline void TriggerSet::make2DPlot(TEfficiency* eff){
 	//GET EFFICIENCIES ON PLOT
 	TCanvas* cv = new TCanvas("cv","cv",800,600);
 	cv->cd();
+	eff->GetZaxis()->SetTitle(m_triggers.at(0).c_str()+" Efficiency");
 	eff->Draw("colz");
 	cv->Update();
 	
@@ -714,7 +715,7 @@ inline void TriggerSet::make2DPlot(TEfficiency* eff){
 	string g_PlotTitle = m_samplename+" Trigger Efficiencies";
 	gr->GetXaxis()->SetTitle("Muon pT (GeV)");
 	gr->GetYaxis()->SetTitle("Muon #eta");
-	gr->GetZaxis()->SetTitle(m_trigger.at(0).c_str()+" Efficiency");
+	// gr->
 
 	cv->Update();
 	gr->Draw();
