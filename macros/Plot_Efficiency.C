a31e015ab43c0ee082591154e9829ccec1aa3e76
Margaret Lazarovits <mlazarovits@gmail.com>
2020-03-04 10:13:18 -0600

    test cuts

--- macros/Plot_Efficiency.C
+++ macros/Plot_Efficiency.C
@@ -184,7 +184,7 @@ else if(sampleName=="zz2l2nu17" && physicsobj=="Muon"){
 
 	// vector<TEfficiency*> zz2l2nu17_mu_effs;
 	TEfficiency* zz2l2nu17_mu_eff;
-	TriggerSet zz2l2nu17_mu(f_zz2l2nu17,true);
+	TriggerSet zz2l2nu17_mu(f_zz2l2nu17,false);
 	zz2l2nu17_mu.SetSampleName("ZZ to 2L2Nu 2017 No ID");
 
 	zz2l2nu17_mu.AddTrigger("HLT_DoubleMu3_DZ_PFMET50_PFMHT60");
@@ -193,7 +193,7 @@ else if(sampleName=="zz2l2nu17" && physicsobj=="Muon"){
 
 	zz2l2nu17_mu.SetVar("Muon_pt");
 	zz2l2nu17_mu.SetCuts("DoubleMuon");
-	zz2l2nu17_mu.SetOutputName("ZZ2L2Nu17_DoubleMuMETTrigs_DoubleMu_METcut200.root");
+	zz2l2nu17_mu.SetOutputName("TESTCUTSZZ2L2Nu17_DoubleMuMETTrigs_DoubleMu_METcut200.root");
 
 	zz2l2nu17_mu_eff = zz2l2nu17_mu.Analyze2D();
 	zz2l2nu17_mu.make2DPlot(zz2l2nu17_mu_eff);
