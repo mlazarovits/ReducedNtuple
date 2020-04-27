c410d887b5199bcfebef35b918fbc100e807f2a4
Margaret Lazarovits <mlazarovits@gmail.com>
2020-04-27 12:20:12 -0500

    shortened script

--- macros/PlotEfficiency_Filters.C
+++ macros/PlotEfficiency_Filters.C
@@ -14,158 +14,70 @@
 
 using namespace std;
 
-void PlotEfficiency_Filters(TString sampleName){
+void PlotEfficiency_Filters(TFile* file, TString sampleName){
 	if(gSystem->OpenDirectory("plots") == 0){
 		gSystem->mkdir("plots");
 		cout << "Created plots folder." << endl;
 	}
 
 	
-	string gPathname = "/home/t3-ku/mlazarov/Ewkinos/CMSSW_10_6_5/src/ReducedNtuple/";
-	TFile* f_wjets16 = TFile::Open((gPathname+"output/Summer16_94X_WJetsToLNu_EventFilters.root").c_str());
-	TFile* f_wjets17 = TFile::Open((gPathname+"output/Fall17_94X_WJetsToLNu_EventFilters.root").c_str());
-	TFile* f_wjets18 = TFile::Open((gPathname+"output/Autumn18_102X_WJetsToLNu_EventFilters.root").c_str());
+	// string gPathname = "/home/t3-ku/mlazarov/Ewkinos/CMSSW_10_6_5/src/ReducedNtuple/";
+	// TFile* f_wjets16 = TFile::Open((gPathname+"output/Summer16_94X_WJetsToLNu_EventFilters.root").c_str());
+	// TFile* f_wjets17 = TFile::Open((gPathname+"output/Fall17_94X_WJetsToLNu_EventFilters.root").c_str());
+	// TFile* f_wjets18 = TFile::Open((gPathname+"output/Autumn18_102X_WJetsToLNu_EventFilters.root").c_str());
 
-	TFile* f_QCD16 = TFile::Open((gPathname+"output/Summer16_102X_QCD_HT700to1000_EventFilters.root").c_str());
-	TFile* f_QCD17 = TFile::Open((gPathname+"output/Fall17_102X_QCD_HT700to1000_EventFilters.root").c_str());
-	TFile* f_QCD18 = TFile::Open((gPathname+"output/Autumn18_102X_QCD_HT700to1000_EventFilters.root").c_str());
+	// TFile* f_QCD16 = TFile::Open((gPathname+"output/Summer16_102X_QCD_HT700to1000_EventFilters.root").c_str());
+	// TFile* f_QCD17 = TFile::Open((gPathname+"output/Fall17_102X_QCD_HT700to1000_EventFilters.root").c_str());
+	// TFile* f_QCD18 = TFile::Open((gPathname+"output/Autumn18_102X_QCD_HT700to1000_EventFilters.root").c_str());
 
-if(sampleName=="wjets16"){
-	if(f_wjets16 == NULL) return;
-	string name = "WJets16_AllFilters_loose";
-	FilterSet wjets16(f_wjets16);
-	wjets16.SetSampleName(name);
 
-	wjets16.AddFilter("Flag_HBHENoiseFilter");
-	wjets16.AddFilter("Flag_HBHENoiseIsoFilter");
-	wjets16.AddFilter("Flag_globalSuperTightHalo2016Filter");
-	wjets16.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
-	wjets16.AddFilter("Flag_goodVertices");
-	wjets16.AddFilter("Flag_eeBadScFilter");
-	wjets16.AddFilter("Flag_BadPFMuonFilter");
+string name = sampleName_+"AllFilters_loose";
+FilterSet filters(file);
+filters.SetSampleName(name);
 
-	wjets16.SetVar("MET_pt");
-	wjets16.SetOutputName(name+".root");
+if(strstr(sampleName,"16")){
+	filters.AddFilter("Flag_HBHENoiseFilter");
+	filters.AddFilter("Flag_HBHENoiseIsoFilter");
+	filters.AddFilter("Flag_globalSuperTightHalo2016Filter");
+	filters.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
+	filters.AddFilter("Flag_goodVertices");
+	filters.AddFilter("Flag_eeBadScFilter");
+	filters.AddFilter("Flag_BadPFMuonFilter");
+	filters.SetVar("MET_pt");
 
-	vector<TEfficiency*> wjets16_effs = wjets16.Analyze();
-	wjets16.makePlots(wjets16_effs);
+}
+else if(strstr(sampleName,"17")){
+	filters.AddFilter("Flag_HBHENoiseFilter");
+	filters.AddFilter("Flag_HBHENoiseIsoFilter");
+	filters.AddFilter("Flag_globalSuperTightHalo2016Filter");
+	filters.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
+	filters.AddFilter("Flag_goodVertices");
+	filters.AddFilter("Flag_eeBadScFilter");
+	filters.AddFilter("Flag_BadPFMuonFilter");
+	filters.AddFilter("Flag_BadChargedCandidateFilter");
+	filters.SetVar("METFixEE2017_pt");
+}
+else if(strstr(sampleName,"18")){
+	filters.AddFilter("Flag_HBHENoiseFilter");
+	filters.AddFilter("Flag_HBHENoiseIsoFilter");
+	filters.AddFilter("Flag_globalSuperTightHalo2016Filter");
+	filters.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
+	filters.AddFilter("Flag_goodVertices");
+	filters.AddFilter("Flag_eeBadScFilter");
+	filters.AddFilter("Flag_BadPFMuonFilter");
+	filters.AddFilter("Flag_BadChargedCandidateFilter");
+	filters.SetVar("MET_pt");
+
 }
 
+filters.SetOutputName(name+".root");
 
-else if(sampleName=="wjets17"){
-	if(f_wjets17 == NULL) return;
-	string name = "WJets17_AllFilters_loose";
-	FilterSet wjets17(f_wjets17);
-	wjets17.SetSampleName(name);
-
-	wjets17.AddFilter("Flag_HBHENoiseFilter");
-	wjets17.AddFilter("Flag_HBHENoiseIsoFilter");
-	wjets17.AddFilter("Flag_globalSuperTightHalo2016Filter");
-	wjets17.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
-	wjets17.AddFilter("Flag_goodVertices");
-	wjets17.AddFilter("Flag_eeBadScFilter");
-	wjets17.AddFilter("Flag_BadChargedCandidateFilter");
-	wjets17.AddFilter("Flag_BadPFMuonFilter");
-
-	wjets17.SetVar("METFixEE2017_pt");
-	wjets17.SetOutputName(name+".root");
-
-	vector<TEfficiency*> wjets17_effs = wjets17.Analyze();
-	wjets17.makePlots(wjets17_effs);
-}
-
-
-else if(sampleName=="wjets18"){
-	if(f_wjets18 == NULL) return;
-	string name = "WJets18_AllFilters_loose";
-	FilterSet wjets18(f_wjets18);
-	wjets18.SetSampleName(name);
-
-	wjets18.AddFilter("Flag_HBHENoiseFilter");
-	wjets18.AddFilter("Flag_HBHENoiseIsoFilter");
-	wjets18.AddFilter("Flag_globalSuperTightHalo2016Filter");
-	wjets18.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
-	wjets18.AddFilter("Flag_goodVertices");
-	wjets18.AddFilter("Flag_eeBadScFilter");
-	wjets18.AddFilter("Flag_BadChargedCandidateFilter");
-	wjets18.AddFilter("Flag_BadPFMuonFilter");
-
-	wjets18.SetVar("MET_pt");
-	wjets18.SetOutputName(name+".root");
-
-	vector<TEfficiency*> wjets18_effs = wjets18.Analyze();
-	wjets18.makePlots(wjets18_effs);
-}
-
-else if(sampleName=="QCD16"){
-	if(f_QCD16 == NULL) return;
-	string name = "QCD16_AllFilters_loose";
-	FilterSet QCD16(f_QCD16);
-	QCD16.SetSampleName(name);
-
-	QCD16.AddFilter("Flag_HBHENoiseFilter");
-	QCD16.AddFilter("Flag_HBHENoiseIsoFilter");
-	QCD16.AddFilter("Flag_globalSuperTightHalo2016Filter");
-	QCD16.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
-	QCD16.AddFilter("Flag_goodVertices");
-	QCD16.AddFilter("Flag_eeBadScFilter");
-	QCD16.AddFilter("Flag_BadPFMuonFilter");
-
-	QCD16.SetVar("MET_pt");
-	QCD16.SetOutputName(name+".root");
-
-	vector<TEfficiency*> QCD16_effs = QCD16.Analyze();
-	QCD16.makePlots(QCD16_effs);
-}
-	
-
-
-else if(sampleName=="QCD17"){
-	if(f_QCD17 == NULL) return;
-	string name = "QCD17_AllFilters_loose";
-	FilterSet QCD17(f_QCD17);
-	QCD17.SetSampleName(name);
-
-	QCD17.AddFilter("Flag_HBHENoiseFilter");
-	QCD17.AddFilter("Flag_HBHENoiseIsoFilter");
-	QCD17.AddFilter("Flag_globalSuperTightHalo2016Filter");
-	QCD17.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
-	QCD17.AddFilter("Flag_goodVertices");
-	QCD17.AddFilter("Flag_eeBadScFilter");
-	QCD17.AddFilter("Flag_BadChargedCandidateFilter");
-	QCD17.AddFilter("Flag_BadPFMuonFilter");
-
-	QCD17.SetVar("MET_pt");
-	QCD17.SetOutputName(name+".root");
-
-	vector<TEfficiency*> QCD17_effs = QCD17.Analyze();
-	QCD17.makePlots(QCD17_effs);
-}
+vector<TEfficiency*> effs = filters.Analyze();
+filters.makePlots(effs);
 
 
 
 
-else if(sampleName=="QCD18"){
-	if(f_QCD18 == NULL) return;
-	string name = "QCD18_AllFilters_loose";
-	FilterSet QCD18(f_QCD18);
-	QCD18.SetSampleName(name);
-
-	QCD18.AddFilter("Flag_HBHENoiseFilter");
-	QCD18.AddFilter("Flag_HBHENoiseIsoFilter");
-	QCD18.AddFilter("Flag_globalSuperTightHalo2016Filter");
-	QCD18.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
-	QCD18.AddFilter("Flag_goodVertices");
-	QCD18.AddFilter("Flag_eeBadScFilter");
-	QCD18.AddFilter("Flag_BadChargedCandidateFilter");
-	QCD18.AddFilter("Flag_BadPFMuonFilter");
-
-	QCD18.SetVar("MET_pt");
-	QCD18.SetOutputName(name+".root");
-
-	vector<TEfficiency*> QCD18_effs = QCD18.Analyze();
-	QCD18.makePlots(QCD18_effs);
-}
 
 
 else{
