2b2b6296899b9fa3953636127fcd0b0b3455fd16
Margaret Lazarovits <mlazarovits@gmail.com>
2020-04-29 13:37:04 -0500

    updated event filter list

--- macros/PlotEfficiency_Filters.C
+++ macros/PlotEfficiency_Filters.C
@@ -15,22 +15,6 @@
 using namespace std;
 
 void PlotEfficiency_Filters(TString sfile, TString sampleName){
-	// if(gSystem->OpenDirectory("plots") == 0){
-	// 	gSystem->mkdir("plots");
-	// 	cout << "Created plots folder." << endl;
-	// }
-
-	
-	// string gPathname = "/home/t3-ku/mlazarov/Ewkinos/CMSSW_10_6_5/src/ReducedNtuple/";
-	// TFile* f_wjets16 = TFile::Open((gPathname+"output/Summer16_94X_WJetsToLNu_EventFilters.root").c_str());
-	// TFile* f_wjets17 = TFile::Open((gPathname+"output/Fall17_94X_WJetsToLNu_EventFilters.root").c_str());
-	// TFile* f_wjets18 = TFile::Open((gPathname+"output/Autumn18_102X_WJetsToLNu_EventFilters.root").c_str());
-
-	// TFile* f_QCD16 = TFile::Open((gPathname+"output/Summer16_102X_QCD_HT700to1000_EventFilters.root").c_str());
-	// TFile* f_QCD17 = TFile::Open((gPathname+"output/Fall17_102X_QCD_HT700to1000_EventFilters.root").c_str());
-	// TFile* f_QCD18 = TFile::Open((gPathname+"output/Autumn18_102X_QCD_HT700to1000_EventFilters.root").c_str());
-
-
 TString name = sampleName+"_AllFilters_loose";
 TFile* file = TFile::Open(sfile);
 FilterSet filters(file);
@@ -42,7 +26,6 @@ if(strstr(sampleName,"16")){
 	filters.AddFilter("Flag_globalSuperTightHalo2016Filter");
 	filters.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
 	filters.AddFilter("Flag_goodVertices");
-	filters.AddFilter("Flag_eeBadScFilter");
 	filters.AddFilter("Flag_BadPFMuonFilter");
 	filters.SetVar("MET_pt");
 
@@ -53,9 +36,7 @@ else if(strstr(sampleName,"17")){
 	filters.AddFilter("Flag_globalSuperTightHalo2016Filter");
 	filters.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
 	filters.AddFilter("Flag_goodVertices");
-	filters.AddFilter("Flag_eeBadScFilter");
 	filters.AddFilter("Flag_BadPFMuonFilter");
-	filters.AddFilter("Flag_BadChargedCandidateFilter");
 	filters.SetVar("METFixEE2017_pt");
 }
 else if(strstr(sampleName,"18")){
@@ -64,9 +45,7 @@ else if(strstr(sampleName,"18")){
 	filters.AddFilter("Flag_globalSuperTightHalo2016Filter");
 	filters.AddFilter("Flag_EcalDeadCellTriggerPrimitiveFilter");
 	filters.AddFilter("Flag_goodVertices");
-	filters.AddFilter("Flag_eeBadScFilter");
 	filters.AddFilter("Flag_BadPFMuonFilter");
-	filters.AddFilter("Flag_BadChargedCandidateFilter");
 	filters.SetVar("MET_pt");
 
 }
